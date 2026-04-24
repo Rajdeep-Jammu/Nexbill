8a64da7bf812f33fccc2cad156a9aeb74d1583e0
Batman <iambatman.24x7@gmail.com>
2026-04-24 17:15:51 +0000

    setup failed collection is not defined while creting shop

--- src/app/admin/setup/page.tsx
+++ src/app/admin/setup/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/navigation';
 import { motion } from 'framer-motion';
 import { Loader2 } from 'lucide-react';
 import { signInAnonymously } from 'firebase/auth';
-import { doc, setDoc, serverTimestamp } from 'firebase/firestore';
+import { doc, setDoc, serverTimestamp, collection } from 'firebase/firestore';
 
 import { useAuthStore } from '@/hooks/use-auth-store';
 import { Logo } from '@/components/Logo';
