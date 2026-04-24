d6a75cb579c7602530169b96b680b1490c16e206
Batman <iambatman.24x7@gmail.com>
2026-04-24 16:29:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1966643867-701ae",
+  "appId": "1:425775519525:web:05f600a8d014a8f6a52ccc",
+  "apiKey": "AIzaSyBQHdt39JN0PJNH6tU91pc2LapPNUaqVdo",
+  "authDomain": "studio-1966643867-701ae.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "425775519525"
+};
